cb3af14a9640db96c2f076bc4a6092f54120590d
mahin557 <65226286+mahin557@users.noreply.github.com>
2021-12-02 12:21:29 +0600

    Add files via upload

---
+++ assignment2/question6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 6 </title>
+</head>
+<body>
+<?php 
+function BodyMassIndex($num1,$num2){
+$sum=($num1/($num2*$num2));
+
+echo"<h1> Your Body Mass index is $sum";
+}
+
+
+BodyMassIndex(76,125)
+
+
+
+
+?>
+
+</body>
+</html>
